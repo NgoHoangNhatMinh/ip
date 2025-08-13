24841af19b8e7501da4d759b61c49c339991003c
Minh <ngohoangnhatminh@gmail.com>
2025-08-13 22:41:02 +0800

    Implement Level 3

--- src/main/java/Lux.java
+++ src/main/java/Lux.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.List;
 import java.util.ArrayList;
 
 public class Lux {
     public static void main(String[] args) {
-        List<String> texts = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
 
         System.out.println("Hello! I'm LUX\nWhat can I do for you?\n-------------------------------\n");
 
@@ -16,16 +18,34 @@ public class Lux {
                 scanner.close();
                 return;
             } else if (command.equals("list")) {
-                list(texts);
+                list(tasks);
             } else {
-                texts.add(command);
-                System.out.println("* added: " + command);
+                Pattern markPattern = Pattern.compile("mark [\\d]+");
+                Pattern unmarkPattern = Pattern.compile("unmark [\\d]+");
+                if (unmarkPattern.matcher(command).find()) {
+                    int index = Integer.parseInt(command.replaceAll("[^\\d]", ""));
+                    if (index >= tasks.size()) {
+                        System.out.println(String.format("There is no task %d", index));
+                    } else {
+                        tasks.get(index).unmark();
+                    }
+                } else if (markPattern.matcher(command).find()) {
+                    int index = Integer.parseInt(command.replaceAll("[^\\d]", ""));
+                    if (index >= tasks.size()) {
+                        System.out.println(String.format("There is no task %d", index));
+                    } else {
+                        tasks.get(index).mark();
+                    }
+                } else {
+                    tasks.add(new Task(command));
+                    System.out.println("* added: " + command);
+                }
             }
             System.out.println("------------------------------\n");
         }
     }
 
-    private static void list(List<String> texts) {
+    private static void list(List<Task> texts) {
         for (int i = 0; i < texts.size(); i++) {
             String item = String.format("%d. %s", i, texts.get(i));
             System.out.println(item);
