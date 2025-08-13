0da4da7ed21ec9d83f417cf8cd50e6b5271aea79
Minh <ngohoangnhatminh@gmail.com>
2025-08-13 22:04:07 +0800

    Implement Level 2

--- src/main/java/Lux.java
+++ src/main/java/Lux.java
@@ -1,24 +1,34 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Lux {
     public static void main(String[] args) {
-        String greeting = "-------------------------------\n"
-                + "Hello! I'm LUX\n"
-                + "What can I do for you?\n"
-                + "-------------------------------\n";
-        System.out.println(greeting);
+        List<String> texts = new ArrayList<>();
+
+        System.out.println("Hello! I'm LUX\nWhat can I do for you?\n-------------------------------\n");
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String command = scanner.nextLine();
-            System.out.println("-------------------------------");
             if (command.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!\n");
+                System.out.println("* Bye. Hope to see you again soon!\n");
                 scanner.close();
                 return;
+            } else if (command.equals("list")) {
+                list(texts);
+            } else {
+                texts.add(command);
+                System.out.println("* added: " + command);
             }
-            System.out.println(command);
-            System.out.println("-------------------------------");
+            System.out.println("------------------------------\n");
+        }
+    }
+
+    private static void list(List<String> texts) {
+        for (int i = 0; i < texts.size(); i++) {
+            String item = String.format("%d. %s", i, texts.get(i));
+            System.out.println(item);
         }
     }
 }
