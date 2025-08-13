780da50ce96bf0c8aefc4615e6438241885af8da
Minh <ngohoangnhatminh@gmail.com>
2025-08-13 21:36:08 +0800

    Implement Level 0

---
+++ src/main/java/Lux.java
@@ -0,0 +1,9 @@
+public class Lux {
+    public static void main(String[] args) {
+        String logo = "Hello! I'm LUX\n"
+                + "What can I do for you?\n"
+                + "-------------------------------\n"
+                + "Bye. Hope to see you again soon!\n";
+        System.out.println(logo);
+    }
+}
