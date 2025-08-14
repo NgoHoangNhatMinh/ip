4880ed583995a93e8b614ef3c70368442628b8e6
Minh <ngohoangnhatminh@gmail.com>
2025-08-14 12:57:26 +0800

    Implement Level 4
    Added input parser using predefined keywords

---
+++ src/main/java/EventTask.java
@@ -0,0 +1,14 @@
+public class EventTask extends Task {
+    protected String startTime, endTime;
+
+    public EventTask(String description, String startTime, String endTime) {
+        super(description);
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + startTime + " to: " + endTime + ")";
+    }
+}
--- src/main/java/Lux.java
+++ src/main/java/Lux.java
@@ -1,48 +1,99 @@
 import java.util.Scanner;
-import java.util.regex.Matcher;
+import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
 
 public class Lux {
     public static void main(String[] args) {
         List<Task> tasks = new ArrayList<>();
+        Set<String> keywords = new HashSet<>();
+        keywords.add("list");
+        keywords.add("deadline");
+        keywords.add("/by");
+        keywords.add("/from");
+        keywords.add("/to");
+        keywords.add("event");
+        keywords.add("todo");
+        keywords.add("mark");
+        keywords.add("unmark");
+        keywords.add("bye");
 
         System.out.println("Hello! I'm LUX\nWhat can I do for you?\n-------------------------------\n");
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            String command = scanner.nextLine();
-            if (command.equals("bye")) {
-                System.out.println("* Bye. Hope to see you again soon!\n");
-                scanner.close();
-                return;
-            } else if (command.equals("list")) {
-                list(tasks);
-            } else {
-                Pattern markPattern = Pattern.compile("mark [\\d]+");
-                Pattern unmarkPattern = Pattern.compile("unmark [\\d]+");
-                if (unmarkPattern.matcher(command).find()) {
-                    int index = Integer.parseInt(command.replaceAll("[^\\d]", ""));
-                    if (index >= tasks.size()) {
-                        System.out.println(String.format("There is no task %d", index));
-                    } else {
-                        tasks.get(index).unmark();
+            String userInput = scanner.nextLine();
+            Map<String, String> parsedCommand = parseCommand(userInput, keywords);
+
+            for (Map.Entry<String, String> entry : parsedCommand.entrySet()) {
+                String k = entry.getKey(), v = entry.getValue();
+                if (k.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    scanner.close();
+                    return;
+                } else if (k.equals("list")) {
+                    list(tasks);
+                } else if (k.equals("mark")) {
+                    tasks.get(Integer.parseInt(v)).mark();
+                } else if (k.equals("unmark")) {
+                    tasks.get(Integer.parseInt(v)).unmark();
+                } else if (k.equals("add")) {
+                    tasks.add(new Task(userInput));
+                    System.out.println("* added: " + userInput);
+                } else if (k.equals("todo")) {
+                    TodoTask t = new TodoTask(v);
+                    tasks.add(t);
+                    System.out.println("* Got it. I've added this task: \n" + t);
+                } else if (k.equals("deadline")) {
+                    if (parsedCommand.containsKey("/by")) {
+                        DeadlineTask t = new DeadlineTask(v, parsedCommand.get("/by"));
+                        tasks.add(t);
+                        System.out.println("* Got it. I've added this task: \n" + t);
                     }
-                } else if (markPattern.matcher(command).find()) {
-                    int index = Integer.parseInt(command.replaceAll("[^\\d]", ""));
-                    if (index >= tasks.size()) {
-                        System.out.println(String.format("There is no task %d", index));
-                    } else {
-                        tasks.get(index).mark();
+                } else if (k.equals("event")) {
+                    if (parsedCommand.containsKey("/from") && parsedCommand.containsKey("/to")) {
+                        EventTask t = new EventTask(v, parsedCommand.get("/from"), parsedCommand.get("/to"));
+                        tasks.add(t);
+                        System.out.println("* Got it. I've added this task: \n" + t);
                     }
-                } else {
-                    tasks.add(new Task(command));
-                    System.out.println("* added: " + command);
                 }
             }
+
             System.out.println("------------------------------\n");
         }
+    }
+
+    // Parse command into a map of {commands: arguments}
+    private static Map<String, String> parseCommand(String userInput, Set<String> keywords) {
+        Map<String, String> parsed = new LinkedHashMap<>();
+        String[] tokens = userInput.split(" ");
+
+        String key = null;
+        String value = "";
+
+        for (String t : tokens) {
+            if (keywords.contains(t)) {
+                if (key != null) {
+                    parsed.put(key, value.trim());
+                }
+                key = t;
+                value = "";
+            } else {
+                value += t + " ";
+            }
+        }
+
+        if (key != null) {
+            parsed.put(key, value.trim());
+        } else {
+            parsed.put("add", value.trim());
+        }
+
+        return parsed;
     }
 
     private static void list(List<Task> texts) {
